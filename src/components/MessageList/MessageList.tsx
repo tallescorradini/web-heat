ba9f44abaa0599af988b4453cffea8687433668d
Talles Corradini <tallescorradini@gmail.com>
2021-10-19 21:15:38 -0300

    feat (MessageList): add elements and style according to design

--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -1,3 +1,30 @@
+import styles from "./MessageList.module.scss";
+
+import logoImg from "../../assets/logo.svg";
+
 export function MessageList() {
-  return <h1>MessageList</h1>;
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+      <ul className={styles.messageList}>
+        {[1, 2, 3].map((key) => (
+          <li className={styles.message}>
+            <p className={styles.MessageContent}>
+              Não vejo a hora de começar esse evento, com certeza vai ser o
+              melhor de todos os tempos, vamooo pra cima! 🔥🔥
+            </p>
+            <div className={styles.messageUser}>
+              <div className={styles.userImage}>
+                <img
+                  src="https://github.com/tallescorradini.png"
+                  alt="Talles Corradini"
+                />
+              </div>
+              <span>Talles Corradini</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
