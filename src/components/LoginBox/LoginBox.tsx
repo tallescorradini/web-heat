6e63ce4ff01a923dcde47bf8ebe8631ee7fdcd89
Talles Corradini <tallescorradini@gmail.com>
2021-10-19 21:57:07 -0300

    feat (LoginBox): implement user signin functionality

--- src/components/LoginBox/LoginBox.tsx
+++ src/components/LoginBox/LoginBox.tsx
@@ -1,12 +1,51 @@
+import { useEffect } from "react";
 import { VscGithubInverted } from "react-icons/vsc";
+import { api } from "../../services/api";
 
 import styles from "./LoginBox.module.scss";
 
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  };
+};
+
 export function LoginBox() {
+  const signInUrl = `http://localhost:4000/github`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>("authenticate", {
+      code: githubCode,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem("@dowhile:token", token);
+
+    console.log(user);
+  }
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes("?code=");
+
+    if (hasGithubCode) {
+      const [baseUrl, githubCode] = url.split("?code=");
+
+      window.history.pushState({}, "", baseUrl);
+
+      signIn(githubCode);
+    }
+  }, []);
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24px" />
         Entrar com Github
       </a>
